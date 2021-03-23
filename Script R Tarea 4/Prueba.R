5966cc325a4caad12cb0cbf967b15e0d13e0d09c
Adrian Curet <67012884+Adhoper@users.noreply.github.com>
2021-03-22 20:06:56 -0400

    Update Prueba.R

--- Script R Tarea 4/Prueba.R
+++ Script R Tarea 4/Prueba.R
@@ -40,10 +40,10 @@ ggplot2::ggplot(archivo,aes(x=RECINTO)) + geom_bar(fill="red")
 download.file(url ="https://www.itsc.edu.do/transparencia/datosabiertos/compras/ordenes-de-compra-2018-2019.xlsx", destfile = "Ordenes-de-compra-2018-2019.xlsx", mode = "wb")
 
 #Se importa el archivo excel con los datos.
-archivo <- readxl::read_excel("Ordenes-de-compra-2018-2019.xlsx")
+archivos <- readxl::read_excel("Ordenes-de-compra-2018-2019.xlsx")
 
 #Se crea la grafica por Modalidad
-ggplot2::ggplot(archivo,aes(x=Modalidad)) + geom_bar(fill="blue")
+ggplot2::ggplot(archivos,aes(x=Modalidad)) + geom_bar(fill="blue")
 
 #Este tiene datos de ordenes de compra del 2018 y 2019 donde en el grafico
 #mostrara la cantidad de compras en las cuales se dividiran en modalidades.
@@ -56,10 +56,10 @@ ggplot2::ggplot(archivo,aes(x=Modalidad)) + geom_bar(fill="blue")
 download.file(url ="https://inabie.gob.do/transparencia/index.php/datos-abiertos/category/1702-2020?download=7878:estudiantes-beneficiados-con-utileria-escolar-2020", destfile = "Estudiantes Beneficiados con Utileria Escolar 2020.xlsx", mode = "wb")
 
 #Se importa el archivo excel con los datos.
-archivo <- readxl::read_excel("Estudiantes Beneficiados con Utileria Escolar 2020.xlsx")
+archivoss <- readxl::read_excel("Estudiantes Beneficiados con Utileria Escolar 2020.xlsx")
 
 #Se crea la grafica por Tanda
-ggplot2::ggplot(archivo,aes(x=TANDA)) + geom_bar(fill="blue")
+ggplot2::ggplot(archivoss,aes(x=TANDA)) + geom_bar(fill="blue")
 
 #Este tiene datos de estudiantes que se beneficiaron con utileria escolar
 #donde la grafica se dividira en mostrar la cantidad de estudiantes
